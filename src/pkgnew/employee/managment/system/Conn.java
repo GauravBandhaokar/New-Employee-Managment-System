5e54ed679ae9407f364e354d1981985a03d21369
GauravBandhaokar <91546077+GauravBandhaokar@users.noreply.github.com>
2023-01-17 11:30:51 +0530

    Initial commit

---
+++ src/pkgnew/employee/managment/system/Conn.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgnew.employee.managment.system;
+
+/**
+ *
+ * @author Gaurav Bandhaokar
+ */
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///newemployeemanagmentsystem", "root", "Gaurav@123");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
